7943233c970969592e4c63370f91661138a1877e
arioanindito <arioanindito@hotmail.com>
2022-04-08 06:36:24 +0300

    modified

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,10 @@ import Feed from "../components/Feed";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
 import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
 import { connectToDatabase } from "../util/mongodb";
 
-export default function Home({ posts }) {
+export default function Home({ posts, articles }) {
 	const [modalOpen, setModalOpen] = useRecoilState(modalState);
 	const [modalType, setModalType] = useRecoilState(modalTypeState);
 	const router = useRouter();
@@ -38,7 +39,7 @@ export default function Home({ posts }) {
 					<Sidebar />
 					<Feed posts={posts} />
 				</div>
-				{/* Widgets */}
+				<Widgets articles={articles} />
 				<AnimatePresence>
 					{modalOpen && (
 						<Modal handleClose={() => setModalOpen(false)} type={modalType} />
@@ -69,11 +70,15 @@ export async function getServerSideProps(context) {
 		.sort({ timestamp: -1 })
 		.toArray();
 
-	//Get Google News API
+	// Get Google News API
+	const results = await fetch(
+		`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+	).then((res) => res.json());
 
 	return {
 		props: {
 			session,
+			articles: results.articles,
 			posts: posts.map((post) => ({
 				_id: post._id.toString(),
 				input: post.input,
